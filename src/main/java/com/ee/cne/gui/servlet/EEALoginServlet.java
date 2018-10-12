a783887a88f0da7b35e958f811167782f8557198
Soumen Choudhury <sou.tcs@gmail.com>
2018-10-12 14:28:47 +0530

    google code format

--- src/main/java/com/ee/cne/gui/servlet/EEALoginServlet.java
+++ src/main/java/com/ee/cne/gui/servlet/EEALoginServlet.java
@@ -2,7 +2,6 @@ package com.ee.cne.gui.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
@@ -12,31 +11,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/eea")
-@ServletSecurity(@HttpConstraint(rolesAllowed = { "cc-user", "ccl-user", "soc-user" }))
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"cc-user", "ccl-user", "soc-user"}))
 public class EEALoginServlet extends HttpServlet {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String id = request.getParameter("context");
-		System.out.println(id);
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String id = request.getParameter("context");
+    System.out.println(id);
 
-		PrintWriter writer = response.getWriter();
+    PrintWriter writer = response.getWriter();
 
-		// build HTML code
-		String htmlRespone = "<html>";
-		htmlRespone += "<h2 align='center'>Your username is : " + request.getHeader("HTTP_SM_UID") + "<br/>";
-		htmlRespone += "And Your ROLEs are : " + request.getHeader("HTTP_SM_ROLES") + "<br/>";
-		htmlRespone += " SM LOGIN Successful</h2>";
-		htmlRespone += "</html>";
+    // build HTML code
+    String htmlRespone = "<html>";
+    htmlRespone +=
+        "<h2 align='center'>Your username is : " + request.getHeader("HTTP_SM_UID") + "<br/>";
+    htmlRespone += "And Your ROLEs are : " + request.getHeader("HTTP_SM_ROLES") + "<br/>";
+    htmlRespone += " SM LOGIN Successful</h2>";
+    htmlRespone += "</html>";
 
-		request.getSession(true).invalidate();
-		// return response
-		writer.println(htmlRespone);
-	}
+    request.getSession(true).invalidate();
+    // return response
+    writer.println(htmlRespone);
+  }
 
 }
